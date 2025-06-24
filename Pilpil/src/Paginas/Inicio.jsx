1b3d98d09adfda86e3652df962c5db95709aaabe
AgustinGonzalez001 <agustin4147@gmail.com>
2025-06-23 21:02:45 -0300

    ultimo intento de subir el proyecto

--- Pilpil/src/Paginas/Inicio.jsx
+++ Pilpil/src/Paginas/Inicio.jsx
@@ -1,9 +1,10 @@
+import Pilpil from '../assets/Pilpil.png'
 function Inicio(){
     return(
         <>
             <div className='w-335 static'>
-                <img className='md:w-335 w-full flex justify-end  '
-                    src="https://scontent.fcrd6-1.fna.fbcdn.net/v/t39.30808-6/277752195_351430970334567_6425290526084775063_n.png?_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=2JbjwWOHWVUQ7kNvwF3h87S&_nc_oc=Adnmier7dAWuBBUtFVWwBOz_4IQZm2GIR4sSxBVOrVMmhgPTMYhythPVW4Rcj5qeHzk&_nc_zt=23&_nc_ht=scontent.fcrd6-1.fna&_nc_gid=9z4mJIb54MVzOs5w6A0FYg&oh=00_AfNlPWIS5UZJpA8uJo6tAYxYkGKOuv0uYVAoDLZZdOWcAw&oe=6857C31B" alt="" />
+                <img className='md:w-335 w-full flex justify-end'
+                    src={Pilpil} alt="imagen PilPil" />
                 
                 <div className="w-50 h-35 md:w-75 md:h-30 font-mono md:text-lg text-xs  bg-[#ffc8007d] rounded-[2vh] md:absolute md:top-50 md:right-20 top-35 rigth-5  grid justify-center items-center justify-items-center">
                     <p >¡Abierto todos los dias!</p>
